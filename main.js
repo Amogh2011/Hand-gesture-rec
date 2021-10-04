a8ce0ee4052f667e5db0905440d04cc400137073
Amogh2011 <76557885+Amogh2011@users.noreply.github.com>
2021-10-04 21:19:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+prediction_1 = ""
+prediction_2 = ""
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+
+function take_snapshot()
+
+camera = document.getElementById("result").innerHTML = '<img id="captured_image" src"'+data_uri+'"/>';
